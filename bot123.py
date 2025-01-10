24b2535ff3079e92f472c5449044ea898b97c155
juls45 <julianamojicabenavides@gmail.com>
2025-01-10 16:53:21 -0500

    Update bot123.py

--- bot123.py
+++ bot123.py
@@ -1,21 +1,76 @@
 import discord
 from discord.ext import commands
+import random
+
+description = '''This is a basic bot i just hope it helps .
+
+There are a number of utility commands being showcased here.'''
 
 intents = discord.Intents.default()
+intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
 
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('-----------')
+
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send(f'Hola, soy un bot {bot.user}!')
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
 
 @bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
 
-bot.run("EL TOKEN SECRETO VA AQUÍ")
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} pitifully he joined {discord.utils.format_dt(member.joined_at)}')
+
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} you are wrong')
+
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """Is the bot amazing?"""
+    await ctx.send('no')
+
+
+bot.run('token')
